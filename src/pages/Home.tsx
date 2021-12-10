928d09727070a1ac6b0c6000643fcc8c70d81059
Heath Milligan <heath@optionstrat.com>
2021-12-09 21:07:12 -0700

    proof of concept

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,25 +1,56 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import {
+  IonButton,
+  IonContent,
+  IonHeader,
+  IonInput,
+  IonMenu,
+  IonModal,
+  IonPage,
+  IonTitle,
+  IonToolbar
+} from '@ionic/react';
+import {menuController} from '@ionic/core';
 import './Home.css';
+import {useState} from "react";
 
 const Home: React.FC = () => {
-  return (
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+
+  return (<>
+    <IonMenu menuId="menu1" swipeGesture={true} contentId="main">
+      <IonContent>
+          <IonTitle>Menu</IonTitle>
+          <IonInput placeholder="This textbox is required"/>
+          <IonButton onClick={() => setIsModalOpen(true)}>Open Modal</IonButton>
+      </IonContent>
+    </IonMenu>
+    <IonModal isOpen={isModalOpen} onDidDismiss={() => setIsModalOpen(false)}>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Modal</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+        <IonContent>
+          <IonInput placeholder="Try to type here"/>
+          <p>Press tab and see how the focus is limited to the menu in the background.</p>
+        </IonContent>
+    </IonModal>
     <IonPage>
       <IonHeader>
         <IonToolbar>
           <IonTitle>Blank</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent fullscreen>
+      <IonContent id="main">
         <IonHeader collapse="condense">
           <IonToolbar>
             <IonTitle size="large">Blank</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer />
+        <IonButton onClick={() => menuController.open('menu1')}>Show Menu</IonButton>
       </IonContent>
     </IonPage>
-  );
+  </>);
 };
 
 export default Home;
